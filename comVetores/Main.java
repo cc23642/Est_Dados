03237697084df9e9daa6334fc29fc0b25079317b
MURILO DOS SANTOS CELA <u23642@LAPA06.academico.cotuca.unicamp.br>
2025-02-25 18:16:26 -0300

    terminando estrutura com vetor

---
+++ comVetores/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[]args){
+        Fila fila = new Fila<Integer>();
+
+        for(int i=0;i<=1000000;i++){
+            try {
+                fila.incluaUmItem(i);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        for(int i=0;i<1000000;i++){
+            try {
+                fila.removaUmItem();
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        System.out.println("acabou!---------");
+    }
+}
